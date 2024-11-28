9ef7e603f2e73d09c0a1721b5e26aa648cbfca60
G-Aleksandra-git <alexandrag63248@example.com>
2024-11-28 07:43:08 +0300

    Задание 2

--- hw-2.js
+++ hw-2.js
@@ -1,6 +1,7 @@
-let a = 10;
+let a = 10;  // Задание.1
 alert(a);
 a = 20;       // Переопределяем значение переменной a на 20
 alert(a);
-
+const releaseIPhone = 'Год выпуска нового iPhone: 2007 год'; // Задание.2
+alert(releaseIPhone); 
 
