4784b5ddd7825ed36ea807bb5f3cce42e9101611
G-Aleksandra-git <alexandrag63248@example.com>
2024-12-27 16:55:15 +0300

    добавлен цикл продолжения игры

--- kamen.js
+++ kamen.js
@@ -1,35 +1,47 @@
 
 function startRockPaperScissors() {
-    const options = ["камень", "ножницы", "бумага"]; 
-  
-    let userChoice = prompt("Выбери: камень, ножницы или бумага").toLowerCase();//ввод игрока
-  
+    const options = ["камень", "ножницы", "бумага"];
     
-    if (!options.includes(userChoice)) {// Проверяем ввод
-      alert("Ты ввел что-то неверное. Попробуй еще раз.");
-      return;
+      //для повторного запуска игры
+    let playAgain = true;
+    
+    while (playAgain) {        
+        let userChoice = prompt("Выбери: камень, ножницы или бумага").toLowerCase();
+            
+        if (!options.includes(userChoice)) {// Проверяем правильность ввода
+          alert("Ты ввел что-то не то... Попробуйте снова.");
+          continue; // идем к началу цикла
+        }
+    
+        // выбор компьютера
+        const computerChoice = options[Math.floor(Math.random() * options.length)];
+    
+        // Определяем победителя
+        let result = "";
+        if (userChoice === computerChoice) {
+          result = "победила дружба! :)";
+        } else if (
+          (userChoice === "камень" && computerChoice === "ножницы") ||
+          (userChoice === "ножницы" && computerChoice === "бумага") ||
+          (userChoice === "бумага" && computerChoice === "камень")
+        ) {
+          result = "ты победил! Поздравляю! :)";
+        } else {
+          result = "увы, ты проиграл";
+        }
+    
+        // показываем результаты
+        alert(
+          `Ты выкинул "${userChoice}".\nЯ выкинул "${computerChoice}".\nПоэтому ${result}!`
+        );
+    
+        // Спрашиваем, хочет ли сыграть еще раз
+        playAgain = confirm("Хочешь еще разок? :)");
     }
-      
-    const computerChoice = options[Math.floor(Math.random() * options.length)];// выбор компьютера
+    
+      // Сообщение после завершения всех игр
+    alert("Спасибо за игру! Приходи еще!");
+}
   
-    // Определяем победителя
-    let result = "";
-    if (userChoice === computerChoice) {
-      result = "у нас ничья!";
-    } else if (
-      (userChoice === "камень" && computerChoice === "ножницы") ||
-      (userChoice === "ножницы" && computerChoice === "бумага") ||
-      (userChoice === "бумага" && computerChoice === "камень")
-    ) {
-      result = "ты проиграл.";
-    } else {
-      result = "ты выиграл! :)";
-    }
-  
-    // Выводим результаты
-    alert(
-      `Ты выкинул "${userChoice}".\nА я "${computerChoice}".\nПоэтому ${result}`
-    );
-  }
   
  
